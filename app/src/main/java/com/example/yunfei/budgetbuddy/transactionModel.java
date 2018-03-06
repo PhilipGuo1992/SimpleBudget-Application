d4eb0c0da1f39988b58b4b53750d8db4c9a2ee84
Unknown <philipguo1992@gmail.com>
2018-03-06 00:04:28 -0400

    create transaction  model

---
+++ app/src/main/java/com/example/yunfei/budgetbuddy/transactionModel.java
@@ -0,0 +1,18 @@
+package com.example.yunfei.budgetbuddy;
+
+/**
+ * Created by yunfei on 2018-03-06.
+ */
+
+public class transactionModel {
+    // transaction type
+    private String budgetType;
+    private String name;
+    private double amount;
+
+    // use date or string ?
+    private String DATE;
+
+    private String notes;
+
+}
