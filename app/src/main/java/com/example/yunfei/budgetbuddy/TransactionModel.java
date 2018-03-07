42c3582220c00ecf2f7c84cc52becc517627b0a1
Unknown <philipguo1992@gmail.com>
2018-03-06 20:27:22 -0400

    add fragment.class

--- app/src/main/java/com/example/yunfei/budgetbuddy/TransactionModel.java
+++ app/src/main/java/com/example/yunfei/budgetbuddy/TransactionModel.java
@@ -4,7 +4,7 @@ package com.example.yunfei.budgetbuddy;
  * Created by yunfei on 2018-03-06.
  */
 
-public class transactionModel {
+public class TransactionModel {
     // transaction type
     private String budgetType;
     private String name;
