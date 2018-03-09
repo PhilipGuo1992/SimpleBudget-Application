e958eb991966d35aa498d6aebfb25806abb02741
Unknown <philipguo1992@gmail.com>
2018-03-08 22:21:46 -0400

    Merge branch 'feature/set_up_fragments' into develop
    design AddBudget layout

--- app/src/main/java/com/example/yunfei/budgetbuddy/AddBudgetActivity.java
+++ app/src/main/java/com/example/yunfei/budgetbuddy/AddBudgetActivity.java
@@ -2,12 +2,26 @@ package com.example.yunfei.budgetbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddBudgetActivity extends AppCompatActivity {
+
+    private EditText budgetName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_budget);
+    }
+
+
+
+
+
+    public void onRadioButtonClicked(View view) {
+        // the radio button
+        
     }
 }
